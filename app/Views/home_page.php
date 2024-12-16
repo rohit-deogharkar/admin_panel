d09efb33b90f434a0ffcd5935a29c8a22c7c364a
rohit-deogharkar <rohitdeogharkar03@gmail.com>
2024-12-16 20:18:55 +0530

    project initiate

---
+++ app/Views/home_page.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    
+    <link rel="stylesheet" href="<?= base_url('css/bootstrap.min.css')?>">
+    <script src="<?= base_url('js/bootstrap.min.js')?>"></script>
+</head>
+<body>
+    <h6 class="m-0 text-center"></h6>
+</body>
+<script>
+    let time = new Date()
+    console.log(time)
+</script>
+</html>
